d96389722c7b535483ceb2fd09b8f95285863bed
santiago-rodrig <santo1996.29@gmail.com>
2021-02-25 19:03:54 -0400

    add article upvotes endpoint

--- src/server.js
+++ src/server.js
@@ -1,20 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+  },
+  "learn-node": {
+    upvotes: 0,
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+  },
+};
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello");
-});
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
 
-app.get("/hello/:name", (req, res) => {
-  res.send(`Hello ${req.params.name}`);
-});
+  articlesInfo[articleName].upvotes += 1;
 
-app.post("/hello", (req, res) => {
-  res.send(`Hello ${req.body.name}`);
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
 });
 
 app.listen(8000, () => console.log("Listening on port 8000"));
