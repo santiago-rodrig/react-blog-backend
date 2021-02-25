d820ae21dd58a5ac8e27530e019357b6ab7b3488
santiago-rodrig <santo1996.29@gmail.com>
2021-02-25 19:39:23 -0400

    connect express and mongodb

--- src/server.js
+++ src/server.js
@@ -1,24 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-    comments: [],
-  },
-  "learn-node": {
-    upvotes: 0,
-    comments: [],
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-    comments: [],
-  },
-};
+import { MongoClient } from "mongodb";
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const db = client.db("react-blog-backend");
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+});
 
 app.post("/api/articles/:name/upvote", (req, res) => {
   const articleName = req.params.name;
@@ -36,7 +44,6 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
   const articleName = req.params.name;
 
   articlesInfo[articleName].comments.push(req.body);
-
   res.status(200).send(articlesInfo[articleName]);
 });
 
